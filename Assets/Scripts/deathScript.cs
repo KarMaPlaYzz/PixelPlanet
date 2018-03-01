c077c2914af175603c8222e70d3077a7b21dc1e1
KarMaPlaYzz <karmaplayzz2002@gmail.com>
2018-03-01 01:29:26 +0100

    ASdfcghjk

--- Assets/Scripts/deathScript.cs
+++ Assets/Scripts/deathScript.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class deathScript : MonoBehaviour {
 
+    private GameObject landerShip;
+
+    private void Start()
+    {
+        landerShip = GameObject.Find("LanderShip");
+    }
+
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Death")
         {
-            Debug.Log("HIT!");
+            landerShip.SetActive(false);
         }
     }
 }
---
+++ Assets/Scripts/ellipticalMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ellipticalMovement : MonoBehaviour {
+
+	public float centreX;
+	public float centreY;
+	public float alpha;
+	public float horizontalRadius;
+	public float verticalRadius;
+	public float speed;
+
+	void Start()
+	{
+		centreX = this.transform.parent.transform.position.x;
+		centreY=this.transform.parent.transform.position.y;
+	}
+	void Update()
+	{
+		this.transform.position=new Vector2(centreX+(horizontalRadius*Mathf.Cos(alpha*0.005f)),centreY+(verticalRadius*Mathf.Sin(alpha*0.005f)));
+		alpha += speed;
+	}
+}
