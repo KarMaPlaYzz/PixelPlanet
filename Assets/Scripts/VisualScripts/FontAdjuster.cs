0d55ca7ab22cba50b0888178a17bb3bfecd86f80
Max <panoskal@windowslive.com>
2018-03-04 21:01:56 +0200

    MainMenuUI

---
+++ Assets/Scripts/VisualScripts/FontAdjuster.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FontAdjuster : MonoBehaviour {
+
+	public Text AboutUsText;
+
+	Text thisobject;
+	int fontsize;
+
+	void Update () {
+
+		thisobject = gameObject.GetComponent<Text> ();
+		fontsize = AboutUsText.cachedTextGenerator.fontSizeUsedForBestFit;
+		thisobject.fontSize = fontsize;
+	}
+}
